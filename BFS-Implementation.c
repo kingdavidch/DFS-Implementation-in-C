6dff9e68fc80270f0d973dc5ea49377995029729
King Chukwumere <132105674+kingdavidch@users.noreply.github.com>
2025-01-22 18:36:08 +0200

    Update BFS-Implementation.c

--- BFS-Implementation.c
+++ BFS-Implementation.c
@@ -1,143 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-// Structure to represent a queue node
-struct QueueNode {
-    int data;
-    struct QueueNode* next;
-};
+#define MAX_VERTICES 13
 
-// Structure to represent a queue
-struct Queue {
-    struct QueueNode *front, *rear;
-};
-
-// Function to create a new queue node
-struct QueueNode* newQueueNode(int k) {
-    struct QueueNode *temp = (struct QueueNode*)malloc(sizeof(struct QueueNode));
-    temp->data = k;
-    temp->next = NULL;
-    return temp;
-}
-
-// Function to create an empty queue
-struct Queue* createQueue() {
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = q->rear = NULL;
-    return q;
-}
-
-// Function to add a key k to q
-void enQueue(struct Queue* q, int k) {
-    // Create a new LL node
-    struct QueueNode* temp = newQueueNode(k);
-
-    // If queue is empty, then new node is front and rear both
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return;
+void DFS(int graph[MAX_VERTICES][MAX_VERTICES], bool visited[], int vertex, int V) {
+    printf("%d ", vertex);
+    visited[vertex] = true;
+    
+    for (int i = 1; i < V; i++) {
+        if (graph[vertex][i] && !visited[i]) {
+            DFS(graph, visited, i, V);
+        }
     }
-
-    // Add the new node at the end of queue and change rear
-    q->rear->next = temp;
-    q->rear = temp;
 }
 
-// Function to remove a key from given queue q
-int deQueue(struct Queue* q) {
-    // If queue is empty, return NULL.
-    if (q->front == NULL)
-        return -1;
+int main() {
+    int V = 13; // Number of vertices
+    int graph[MAX_VERTICES][MAX_VERTICES] = {0};
+    
+    // Add edges according to the given graph
+    graph[1][2] = graph[2][1] = 1;
+    graph[1][3] = graph[3][1] = 1;
+    graph[2][5] = graph[5][2] = 1;
+    graph[2][4] = graph[4][2] = 1;
+    graph[3][4] = graph[4][3] = 1;
+    graph[3][6] = graph[6][3] = 1;
+    graph[4][7] = graph[7][4] = 1;
+    graph[5][7] = graph[7][5] = 1;
+    graph[5][9] = graph[9][5] = 1;
+    graph[6][8] = graph[8][6] = 1;
+    graph[7][10] = graph[10][7] = 1;
+    graph[8][10] = graph[10][8] = 1;
+    graph[9][11] = graph[11][9] = 1;
+    graph[10][12] = graph[12][10] = 1;
+    graph[11][12] = graph[12][11] = 1;
 
-    // Store previous front and move front one node ahead
-    struct QueueNode* temp = q->front;
-    q->front = q->front->next;
-
-    // If front becomes NULL, then change rear also as NULL
-    if (q->front == NULL)
-        q->rear = NULL;
-
-    int data = temp->data;
-    free(temp);
-    return data;
+    bool visited[MAX_VERTICES] = {false};
+    
+    printf("DFS starting from vertex 1: ");
+    DFS(graph, visited, 1, V);
+    
+    return 0;
 }
-
-// Structure to represent an adjacency list node
-struct AdjListNode {
-    int dest;
-    struct AdjListNode* next;
-};
-
-// Structure to represent an adjacency list
-struct AdjList {
-    struct AdjListNode *head; 
-};
-
-// Structure to represent a graph. A graph is an array of adjacency lists.
-// Size of the array will be V (number of vertices in graph)
-struct Graph {
-    int V;
-    struct AdjList* array;
-};
-
-// Function to create a new adjacency list node
-struct AdjListNode* newAdjListNode(int dest) {
-    struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
-    newNode->dest = dest;
-    newNode->next = NULL;
-    return newNode;
-}
-
-// Function to create a graph of V vertices
-struct Graph* createGraph(int V) {
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
-    graph->V = V;
-
-    // Create an array of adjacency lists. Size of the array will be V
-    graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
-
-     // Initialize each adjacency list as empty by making head as NULL
-    for (int i = 0; i < V; ++i)
-        graph->array[i].head = NULL;
-
-    return graph;
-}
-
-// Adds an edge to an undirected graph
-void addEdge(struct Graph* graph, int src, int dest) {
-    // Add an edge from src to dest. A new node is added to the adjacency list of src.
-    // The node is added at the beginning
-    struct AdjListNode* newNode = newAdjListNode(dest);
-    newNode->next = graph->array[src].head;
-    graph->array[src].head = newNode;
-
-    // Since the graph is undirected, add an edge from dest to src also
-    newNode = newAdjListNode(src);
-    newNode->next = graph->array[dest].head;
-    graph->array[dest].head = newNode;
-}
-
-// Function to print BFS starting from source s
-void BFS(struct Graph* graph, int s) {
-    struct Queue* queue = createQueue();
-
-    // Mark all the vertices as not visited
-    int *visited = (int *)malloc(graph->V * sizeof(int));
-    for (int i = 0; i < graph->V; i++)
-        visited[i] = 0;
-
-    // Mark the current node as visited and enqueue it
-    visited[s] = 1;
-    enQueue(queue, s);
-
-    while (queue->front != NULL) {
-        // Dequeue a vertex from queue and print it
-        s = deQueue(queue);
-        printf("%d ", s);
-
-        // Get all adjacent vertices of the dequeued vertex s
-        // If a adjacent has not been visited, then mark it visited and enqueue it
-        struct AdjListNode* pCrawl = graph->array[s].head;
-        while (pCrawl) {
-            int adj = pCrawl->dest;
-            if (!visited[adj
